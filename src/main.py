780bacae110df7300d7a9210f139ef2162f81a08
Travis Mann <tmann@gmail.com>
2023-03-04 12:36:11 -0500

    initial edits

---
+++ src/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""main.py: high level selenium navigation logic"""
+
+__author__ = "Travis Mann"
+__version__ = "0.1"
+__maintainer__ = "Travis Mann"
+__email__ = "tmann.eng@gmail.com"
+__status__ = "Development"
+
+
+# --- imports ---
+
+
+# --- global vars ---
+
+
+# --- funcs ---
+
+
+# --- main ---
+if __name__ == "__main__":
+    pass
